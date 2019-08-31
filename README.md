c336d9d6a09c83db4a9fa171753c697289460860
Julian Mann <julian.mann@gmail.com>
2019-08-31 12:03:30 -0500

    Added assets for Jean-Michel-BasquiatSquare

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Playing around with style transfer. Purpose of this repo is to make use of Spell
 
 It uses style transfer code from https://github.com/cysmith/neural-style-tf
 
+
+
+
--- scratch.sh
+++ scratch.sh
@@ -5,3 +5,18 @@
     --framework tensorflow \
     "bash ./runner.sh"
 
+
+./face -resolution 1024 
+
+    parser.add_argument('-m', '--margin', type=int, default=0)
+    parser.add_argument('-r', '--resolution', type=int, default=512)
+
+    # args for neural style
+
+    parser.add_argument('--style_image', type=str, default='kate.jpg')
+    parser.add_argument('--main_style_weight', type=float, default=5000)
+    parser.add_argument('--detail_style_weight', type=float, default=400)
+    parser.add_argument('--max_iterations', type=int, default=500)
+    # parser.add_argument('-l', '--local', action='store_true', default=False)
+    parser.add_argument('-t', '--test_spell',
+                        action='store_true', default=False)
